c93042d090fc472583da88cda8b9bbe88ea58204
Рыманов Ростислав <rostik1990-90@mail.ru>
2023-02-03 23:27:31 +0300

    done

---
+++ yatube_api/api/views.py
@@ -0,0 +1,35 @@
+from rest_framework.generics import get_object_or_404
+from rest_framework import permissions
+from rest_framework import viewsets
+
+from posts.models import Comment, Group, Post
+from .permissions import UserIsAuthor
+from .serializers import PostSerializer, GroupSerializer, CommentSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (permissions.IsAuthenticated, UserIsAuthor)
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = (permissions.IsAuthenticated, UserIsAuthor)
+
+    def get_queryset(self):
+        post_id = self.kwargs.get("post_id")
+        new_queryset = Comment.objects.filter(post=post_id)
+        return new_queryset
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, id=self.kwargs.get("post_id"))
+        serializer.save(author=self.request.user, post=post)
--- yatube_api/posts/admin.py
+++ yatube_api/posts/admin.py
@@ -10,6 +10,17 @@ class PostAdmin(admin.ModelAdmin):
     empty_value_display = '-пусто-'
 
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'description')
+    search_fields = ('description', 'title')
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'post', 'author', 'text', 'created')
+    list_filter = ('author', )
+    search_fields = ('post', 'text')
+
+
 admin.site.register(Post, PostAdmin)
-admin.site.register(Group)
-admin.site.register(Comment)
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Comment, CommentAdmin)
